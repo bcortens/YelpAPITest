f64b7cc9b18856b035db56b8edf5e58c10fa92a4
Benjamin Cortens <bcortens@mail.uoguelph.ca>
2015-10-29 15:19:50 -0400

    Initial Commit

---
+++ YelpSampleAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpSampleAPI
+//
+//  Created by Benjamin Cortens on 2015-10-29.
+//  Copyright © 2015 Benjamin Cortens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
